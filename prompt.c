524f3156cbe82d00ccb5611a6900d7adbbaab4c1
Alec Johnson <alec.johnson@hulu.com>
2018-11-09 17:43:11 -0800

    add handling for running/compiling on Windows32bit

--- prompt.c
+++ prompt.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// include methods for if we run this on windows
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* unused) {}
+
+// otherwise include editline headers
+#else
 #include<editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
 
@@ -11,12 +31,16 @@ int main(int argc, char** argv) {
 
   // infinite repl loop
   while (1) {
+
     // prompt user
     char* input = readline("lispy> ");
+
     // add input to history
     add_history(input);
+
     // echo input
     printf("No, you're a %s\n", input);
+
     // free retrieved input
     free(input);
   }
