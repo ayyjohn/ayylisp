89ea51998d9d20da2fad5809753c816e9bd1e89e
Alec Johnson <alecjohnson55@gmail.com>
2019-02-14 17:40:31 -0800

    add preprocessing to handle compilation on windows

--- prompt.c
+++ prompt.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* handle compilation on Windows */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy
+}
+
+/* fake history function */
+void add_history(char* unused) {}
+/* handle compilation on macOS */
+#else
 #include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
   /* print version and exit info */
