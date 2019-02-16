04486093371ee2ed138e94cf4e1fcb594701bc7d
Alec Johnson <alecjohnson55@gmail.com>
2019-02-15 19:53:11 -0500

    remove defunct eval and eval_op functions

--- parsing.c
+++ parsing.c
@@ -162,49 +162,6 @@ void lval_print(lval* v) {
 /* println for lvals */
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
-lval eval_op(lval x, char* op, lval y) {
-
-  /* if either value is an error, just return it */
-  if (x.type == LVAL_ERR) { return x; }
-  if (y.type == LVAL_ERR) { return y; }
-
-  /* otherwise do math on the values */
-  if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
-  if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
-  if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
-  /* special treatment necessary for division because of infinity */
-  if (strcmp(op, "/") == 0) {
-    return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
-  }
-  return lval_err(LERR_BAD_OP);
-}
-
-lval eval(mpc_ast_t* t) {
-
-  // return numbers immediately
-  if(strstr(t->tag, "number")) {
-    /* check if there's an error in conversion */
-    errno = 0;
-    long x = strtol(t->contents, NULL, 10);
-    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
-  }
-
-  // the operator is always 2nd
-  char* op = t->children[1]->contents;
-
-  // third child may be either a number or expression
-  lval x = eval(t->children[2]);
-
-  // go through remaining children and evaluate recursively
-  int i = 3;
-  while (strstr(t->children[i]->tag, "expr")) {
-    x = eval_op(x, op, eval(t->children[i]));
-    i++;
-  }
-
-  return x;
-}
-
 int main(int argc, char** argv) {
   // create parsers
   mpc_parser_t* Number     = mpc_new("number");
