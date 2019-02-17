e9dbb8349e3992cf7d05fe7d7edcfc414da44098
Alec Johnson <alecjohnson55@gmail.com>
2019-02-17 18:04:40 -0500

    add q-expressions as an option for an expression

--- parsing.c
+++ parsing.c
@@ -281,13 +281,13 @@ int main(int argc, char** argv) {
 
   // define the parsers with the following language
   mpca_lang(MPCA_LANG_DEFAULT,
-  "                                          \
-    number : /-?[0-9]+/ ;                    \
-    symbol : '+' | '-' | '*' | '/' ;         \
-    sexpr  : '(' <expr>* ')' ;               \
-    qexpr  : '{' <expr>* '}' ;               \
-    expr   : <number> | <symbol> | <sexpr> ; \
-    aLisp  : /^/ <expr>* /$/ ;               \
+  "                                                    \
+    number : /-?[0-9]+/ ;                              \
+    symbol : '+' | '-' | '*' | '/' ;                   \
+    sexpr  : '(' <expr>* ')' ;                         \
+    qexpr  : '{' <expr>* '}' ;                         \
+    expr   : <number> | <symbol> | <sexpr> | <qexpr> ; \
+    aLisp  : /^/ <expr>* /$/ ;                         \
   ",
   Number, Symbol, Sexpr, Qexpr, Expr, aLisp); 
   // print version and instructions
