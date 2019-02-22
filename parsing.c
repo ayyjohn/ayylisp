6d7e1f90ba278fc3c44063765e3be26ab96ddede
Alec Johnson <alecjohnson55@gmail.com>
2019-02-21 19:55:06 -0800

    add lval_copy method to facilitate moving lvals in and out of the environment

--- parsing.c
+++ parsing.c
@@ -129,6 +129,38 @@ void lval_del(lval* v) {
   free(v);
 }
 
+lval* lval_copy(lval* v) {
+
+  lval* x = malloc(sizeof(lval));
+  x->type = v-> type;
+
+  switch (v->type) {
+    /* for non-nested lval, just copy contents directly */
+    case LVAL_FUN: x->fun = v->fun; break;
+    case LVAL_NUM: x->num = v->num; break;
+
+    /* copy string lvals with strcpy */
+    case LVAL_ERR:
+      x->err = malloc(strlen(v->err) + 1);
+      strcpy(x->err, v->err); break;
+    case LVAL_SYM:
+      x->sym = malloc(strlen(v->sym) + 1);
+      strcpy(x->sym, v->sym); break;
+
+    /* for nested lvals, copy sub expressions recursively */
+    case LVAL_SEXPR:
+    case LVAL_QEXPR:
+      x->count = v->count;
+      x->cell = malloc(sizeof(lval*) * x->count);
+      for (int i = 0; i < x->count; i++) {
+        x->cell[i] = lval_copy(v->cell[i]);
+      }
+    break;
+  }
+
+  return x;
+}
+
 lval* lval_add(lval* v, lval* x) {
   v->count++;
   v->cell = realloc(v->cell, sizeof(lval*) * v->count);
