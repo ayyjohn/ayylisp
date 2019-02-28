2f25e7c5d9f3196a819f8ff39f06a1b4a63c6005
Alec Johnson <alecjohnson55@gmail.com>
2019-02-27 18:25:02 -0800

    add name for string type lvals

--- parsing.c
+++ parsing.c
@@ -362,6 +362,7 @@ char* ltype_name(int t) {
   case LVAL_NUM: return "Number";
   case LVAL_ERR: return "Error";
   case LVAL_SYM: return "Symbol";
+  case LVAL_STR: return "String";
   case LVAL_SEXPR: return "S-Expression";
   case LVAL_QEXPR: return "Q-Expression";
   default: return "Unknown";
