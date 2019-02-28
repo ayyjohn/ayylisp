db68bb45d3a151306df23a404e09a3c34120294f
Alec Johnson <alecjohnson55@gmail.com>
2019-02-27 19:05:22 -0800

    fix indentation of ltype_name

--- parsing.c
+++ parsing.c
@@ -371,14 +371,14 @@ int lval_eq(lval* x, lval* y) {
 /* method to translate lval enum into human-readable names */
 char* ltype_name(int t) {
   switch (t) {
-  case LVAL_FUN: return "Function";
-  case LVAL_NUM: return "Number";
-  case LVAL_ERR: return "Error";
-  case LVAL_SYM: return "Symbol";
-  case LVAL_STR: return "String";
-  case LVAL_SEXPR: return "S-Expression";
-  case LVAL_QEXPR: return "Q-Expression";
-  default: return "Unknown";
+    case LVAL_FUN: return "Function";
+    case LVAL_NUM: return "Number";
+    case LVAL_ERR: return "Error";
+    case LVAL_SYM: return "Symbol";
+    case LVAL_STR: return "String";
+    case LVAL_SEXPR: return "S-Expression";
+    case LVAL_QEXPR: return "Q-Expression";
+    default: return "Unknown";
   }
 }
 
